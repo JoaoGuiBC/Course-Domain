6c6eac2a795f66fca004e0af6219997468422183
joaoguibc <joaoguibc@gmail.com>
2022-04-11 01:26:49 -0300

    Config: Setted base code for classroom service

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -5,7 +5,15 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
+
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { CoursesService } from '../services/courses.service';
+
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { EnrollmentsService } from '../services/enrollments.service';
+
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
+import { StudentsService } from '../services/students.service';
 
 @Module({
   imports: [
@@ -16,6 +24,16 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    // Resolvers
+    CoursesResolver,
+    EnrollmentsResolver,
+    StudentsResolver,
+
+    // Services
+    CoursesService,
+    EnrollmentsService,
+    StudentsService,
+  ],
 })
 export class HttpModule {}
