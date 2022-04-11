f16f677e0868896c88f53d62864af172fe60a631
joaoguibc <joaoguibc@gmail.com>
2022-04-11 02:11:46 -0300

    Config: Added functionality to list every course, student and enrollment in classroom service

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -1,6 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { StudentsService } from '../../../services/students.service';
+import { EnrollmentsService } from '../../../services/enrollments.service';
 
 import { Student } from '../models/student';
 
 @Resolver(() => Student)
-export class StudentsResolver {}
+export class StudentsResolver {
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollments(@Parent() student: Student) {
+    return this.enrollmentsService.getEnrollmentsByStudent(student.id);
+  }
+}
