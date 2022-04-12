fa4157ff4102901b6a761695758f861e50ebe0b4
joaoguibc <joaoguibc@gmail.com>
2022-04-12 01:57:29 -0300

    Feature: Classroom service automatically creates a new enrollment when a purchase is made in purchase service

--- classroom/src/services/courses.service.ts
+++ classroom/src/services/courses.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
 
 interface CreateCourseParams {
+  slug?: string;
   title: string;
 }
 
@@ -23,12 +24,20 @@ export class CoursesService {
     });
   }
 
-  async createCourse({ title }: CreateCourseParams) {
-    const slug = slugify(title, { lower: true });
+  getCourseBySlug(slug: string) {
+    return this.prisma.course.findUnique({
+      where: {
+        slug,
+      },
+    });
+  }
+
+  async createCourse({ title, slug }: CreateCourseParams) {
+    const courseSlug = slug ?? slugify(title, { lower: true });
 
     const courseAlreadyExists = await this.prisma.course.findUnique({
       where: {
-        slug,
+        slug: courseSlug,
       },
     });
 
@@ -39,7 +48,7 @@ export class CoursesService {
     return this.prisma.course.create({
       data: {
         title,
-        slug,
+        slug: courseSlug,
       },
     });
   }
